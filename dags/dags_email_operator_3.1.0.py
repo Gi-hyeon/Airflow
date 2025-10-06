3d3ffb7714eaed9fbec9352052f2c3ba752ab104
Gi-hyeon <ggh4177@gmail.com>
2025-10-06 20:28:17 +0900

    email operator 3.0~~ upload

--- dags/dags_email_operator_3.1.0.py
+++ dags/dags_email_operator_3.1.0.py
@@ -8,14 +8,14 @@ from airflow.providers.smtp.operators.smtp import EmailOperator
 from airflow.sdk import DAG
 
 with DAG(
-    dag_id="dags_email_operator",
+    dag_id="dags_email_operator_3.1.0",
     schedule="0 8 1 * *",
     start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     send_email_task = EmailOperator(
         task_id='send_email_task',
-        conn_id='conn_smtp_gmail',
+        conn_id='conn_smtp_gmail',      #airflow 3.1.0 이후부터 connection 정보 추가
         to='hmms52@naver.com',
         subject='Airflow SUCESS MAIL',
         html_content='Airflow Workflow SUCESS'
