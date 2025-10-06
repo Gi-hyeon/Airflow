1ed583bd3cab892f1239bb1b58da99ff4a4ffdc7
Gi-hyeon <ggh4177@gmail.com>
2025-10-06 20:05:07 +0900

    email operator upload

---
+++ dags/dags_email_operator.py
@@ -0,0 +1,21 @@
+import datetime
+
+import pendulum
+
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.smtp.operators.smtp import EmailOperator
+from airflow.sdk import DAG
+
+with DAG(
+    dag_id="dags_email_operator",
+    schedule="0 8 1 * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    send_email_task = EmailOperator(
+        task_id='send_email_task',
+        to='hmms52@naver.com',
+        subject='Airflow SUCESS MAIL',
+        html_content='Airflow Workflow SUCESS'
+    )
