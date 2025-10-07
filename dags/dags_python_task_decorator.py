54224682d42cb52c8c1f77db51c87961eb226b2c
Gi-hyeon <ggh4177@gmail.com>
2025-10-07 22:34:03 +0900

    task decorator upload

---
+++ dags/dags_python_task_decorator.py
@@ -0,0 +1,18 @@
+import pendulum
+
+from airflow.sdk import dag, task
+
+with dag(
+    dag_id="dags_python_task_decorator",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+    tags=["example"],
+) as DAG:
+    # [START howto_operator_python]
+    @task(task_id="python_task_1")
+    def print_context(some_input):
+       print(f'{some_input}')
+
+    python_task_1 = print_context("task_decorator execute")     # task 객체가 필요없음
+    # [END howto_operator_python]
